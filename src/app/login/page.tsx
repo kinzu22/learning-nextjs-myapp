a57c7a055349eb82ca93a05776d89e6db1b301af
nhattl <nhattl@hpt.vn>
2023-10-26 18:14:10 +0700

    init project

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Page() {
+    return (
+        <main>
+            <h1>login page</h1>
+            <Link href='/'>Back</Link>
+        </main>
+    );
+}
